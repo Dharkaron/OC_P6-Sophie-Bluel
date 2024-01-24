34fe68a881541c44d53d2e765f08541a153b0760
Dharkaron <152791804+Dharkaron@users.noreply.github.com>
2024-01-24 22:31:44 +0100

    api fetch & filters done

--- FrontEnd/assets/scripts/index.js
+++ FrontEnd/assets/scripts/index.js
@@ -1,46 +1,56 @@
+// Initialisation des variables
+const Works = await getWorks()
+const Categories = await getCategories()
 
-//initialisation des variables
-const gallery = document.querySelector(".gallery")
- 
+//// Création et affichage des éléments du DOM
+const portfolio = document.getElementById("portfolio")
+const categoryFilters = document.createElement("article")
+  categoryFilters.classList.add("filters")
+const gallery = document.createElement("article")
+  gallery.classList.add("gallery")
+
+portfolio.appendChild(categoryFilters)
+portfolio.appendChild(gallery)
 
 
-//recupération des fichiers sur l'API
+
+// Récupération des données sur l'API & stockage dans le cache de la session
+//// Images/projets du portfolio à afficher
 async function getWorks(){ 
-  //// Vérification du cache de la session en cours
-  const sessionCache = sessionStorage.getItem("worksValue")
-  
-    if(!sessionCache){
-      const works = await fetch("http://localhost:5678/api/works").then(works => works.json())
-      //// Transformation des pièces en JSON
-      let worksString = JSON.stringify(works)
-      //// Stockage des informations dans le localStorage
-      sessionStorage.setItem("works", worksString)
-      //// Affichage dans la console
-      console.log(works)
-      console.log(worksString)
-      //// Reconstruction des pièces/objets pour lecture par JS
-      return JSON.parse(worksString)
+  const works = await fetch("http://localhost:5678/api/works").then(works => works.json())
+    //// Transformation des pièces en JSON
+    let workString = JSON.stringify(works)
 
-    } else {
-      return JSON.parse(worksString) 
-    }
+      if(!sessionStorage.getItem("works")){
+        //// Stockage des informations dans le localStorage
+        sessionStorage.setItem("works", workString)
+        //// Reconstruction des pièces/objets pour lecture par JS
+        return JSON.parse(sessionStorage.getItem("works"))
+      } else {
+        return JSON.parse(sessionStorage.getItem("works"))
+      }
+}
 
+//// Catégories du portfolio
+async function getCategories(){
+  const categories = await fetch("http://localhost:5678/api/categories").then(categories => categories.json())
+  return categories
 }
 
 
 
 // Affichage des données récupérées sur l'API (Section "Mes Projets")
-async function displayWorks() {
-  ////Récupère le Return de la fonction
-  const allWorks = await getWorks()
-
+async function displayWorks(Works) {
+  gallery.innerHTML = ""
   //// Création des éléments pour chaque objet de WORKS
-  for(let i = 0; i < allWorks.length; i++){
+  for(let i = 0; i < Works.length; i++){
     const figure = document.createElement("figure")
     const img = document.createElement("img")     
     const figcaption = document.createElement("figcaption")
-    img.src = allWorks[i].imageUrl
-    figcaption.innerText = allWorks[i].title
+
+    img.src = Works[i].imageUrl
+    img.alt = Works[i].title
+    figcaption.innerText = Works[i].title
     
     //// Ajout des éléments au DOM
     gallery.appendChild(figure)
@@ -50,4 +60,57 @@ async function displayWorks() {
   }
 
 }
-displayWorks()
+
+
+
+
+//¤ Filtres des éléments affiché dans le portfolio
+
+// Création et affichage des boutons de filtres par catégories
+async function displayFilters(){
+  //// Par défaut, au premier chargement de la page, afficher l'intégralité de la galerie
+  displayWorks(Works) 
+  ////  Création du bouton d'affichage par défaut
+  const btn0 = document.createElement("button")
+    btn0.classList.add("btn")
+    btn0.classList.add("btn-selected")
+    btn0.id = 0
+    btn0.textContent = "Tous"
+  categoryFilters.appendChild(btn0)
+    //// Action au clic du bouton
+    btn0.addEventListener("click", ()=>{
+      removeFilterSelected()
+      displayWorks(Works)
+      btn0.classList.add("btn-selected")
+    })
+  
+  //// Création des boutons selon les catégories de l'API
+  for(let i=0; i<Categories.length; i++){    
+    const btn = document.createElement("button")
+
+    btn.classList.add("btn")
+    btn.id = Categories[i].id
+    btn.textContent = Categories[i].name
+    categoryFilters.appendChild(btn)
+    //// Gestion des actions au clic pour chaque bouton de filtre
+    btn.addEventListener("click", ()=>{
+      removeFilterSelected()
+      const worksFiltered = Works.filter((work)=>{
+        return work.categoryId == btn.id
+       
+      })
+      displayWorks(worksFiltered)
+      btn.classList.add("btn-selected")
+    })
+  }  
+}
+displayFilters() 
+
+
+// Parcours tout les boutons de filtres, et enlève la sélection
+function removeFilterSelected() {
+  const allButtons = document.querySelectorAll(".btn")
+  allButtons.forEach((button)=>{
+    button.classList.remove("btn-selected")
+  })
+}
--- FrontEnd/assets/scripts/test.js
+++ FrontEnd/assets/scripts/test.js
@@ -1,37 +1,95 @@
-//initialisation des variables
-const gallery = document.querySelector(".gallery")
+// Initialisation des variables Générales
+const Works = await getWorks()
+const Categories = await getCategories()
+
+//// Création et affichage des éléments du DOM
+  const portfolio = document.getElementById("portfolio")
+  const categoryFilters = document.createElement("article")
+    categoryFilters.classList.add("filters")
+  const gallery = document.createElement("article")
+    gallery.classList.add("gallery")
+
+  portfolio.appendChild(categoryFilters)
+  portfolio.appendChild(gallery)
 
 
-//recupération des fichiers sur l'API
-const works = await fetch("http://localhost:5678/api/works").then(works => works.json())
-console.log(works)
 
 
-// Affichage des projets depuis l'API
-for (let i = 0; i < works.length; i++){
+// fetch des données de l'API
+async function getWorks(){
+  const works = await fetch("http://localhost:5678/api/works").then(works => works.json())
+  return works
+}getWorks()
+
+// fetch des catégories
+async function getCategories(){
+  const categories = await fetch("http://localhost:5678/api/categories").then(categories => categories.json())
+  console.log(categories)
+  return categories
+}
+
+
+
+
+// Création et ajout des travaux
+async function displayWorks(Works){ 
+  gallery.innerHTML = ""
+
+  for(let i=0; i<Works.length; i++){
     const figure = document.createElement("figure")
-    const img = document.createElement("img")     
+    const img = document.createElement("img")
+      img.src = Works[i].imageUrl
+      img.alt = Works[i].title
     const figcaption = document.createElement("figcaption")
-    img.src = works[i].imageUrl
-    figcaption.innerText = works[i].title
-    
+     figcaption.innerText = Works[i].title
     
     gallery.appendChild(figure)
     figure.appendChild(img)
     figure.appendChild(figcaption)
+  } 
 }
 
-/* allWorks.forEach((work) => {  
-      const figure = document.createElement("figure")
-      const img = document.createElement("img")     
-      const figcaption = document.createElement("figcaption")
-      img.src = work.imageUrl
-      figcaption.innerText = work.title
-      
-      
-      gallery.appendChild(figure)
-      figure.appendChild(img)
-      figure.appendChild(figcaption)
-      
-  })  */
-  
+
+async function displayCategoryFilter(){
+  displayWorks(Works)
+//Ajout d'eventListener sur chaque bouton
+
+const btn0 = document.createElement("button")
+  btn0.classList.add("btn")
+  btn0.classList.add("btn-selected")
+  btn0.innerText = "Tous"
+  btn0.id = 0
+categoryFilters.appendChild(btn0)
+btn0.addEventListener("click", ()=>{
+  removeFilterSelected()
+  displayWorks(Works)
+  btn0.classList.add("btn-selected")
+})
+
+for(let i=0; i<Categories.length; i++){
+  const btn = document.createElement("button")
+
+    btn.classList.add("btn")
+    btn.innerText = Categories[i].name
+    btn.id = Categories[i].id
+  categoryFilters.appendChild(btn)
+
+  btn.addEventListener("click", ()=>{
+    removeFilterSelected()    
+    const workfiltered = Works.filter((work)=>{
+        return work.categoryId == btn.id
+    })
+    displayWorks(workfiltered)   
+    btn.classList.add("btn-selected")
+  })
+}
+
+}displayCategoryFilter()
+
+
+function removeFilterSelected(){
+const allButtons = document.querySelectorAll(".btn")
+allButtons.forEach((button)=>{
+  button.classList.remove("btn-selected")
+})
+}
